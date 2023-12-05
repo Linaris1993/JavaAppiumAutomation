76a91f2869e62a8920a1cb32eed80f779351204c
Linaris1993 <linakhatmullina@gmail.com>
2023-12-04 20:13:06 -0800

    refactor tests with PageFactory

--- src/lib/ui/android/AndroidSearchPageObject.java
+++ src/lib/ui/android/AndroidSearchPageObject.java
@@ -5,11 +5,11 @@ import lib.ui.SearchPageObject;
 
 public class AndroidSearchPageObject extends SearchPageObject {
     static {
-        SEARCH_INIT_ELEMENT="xpath://lib.ui.android.widget.ImageView[@content-desc='Search Wikipedia']";
+        SEARCH_INIT_ELEMENT="xpath://android.widget.ImageView[@content-desc='Search Wikipedia']";
         SEARCH_INPUT="id:org.wikipedia:id/search_src_text";
         SEARCH_RESULT_BY_SUBSTRING_TPL="xpath://*[@text='{SUBSTRING}']";
         SEARCH_CANCEL_BTN="id:org.wikipedia:id/search_close_btn";
-        SEARCH_RESULT_ELEMENT="xpath://*[@resource-id='org.wikipedia:id/search_results_list']//*[@class='lib.ui.android.view.ViewGroup']";
+        SEARCH_RESULT_ELEMENT="xpath://*[@resource-id='org.wikipedia:id/search_results_list']//*[@class='android.view.ViewGroup']";
         SEARCH_EMPTY_RESULT_ELEMENT="id:org.wikipedia:id/results_text";
         SEARCH_EMPTY_RESULT_LABEL="xpath://*[@text='No results']";
         SEARCH_RESULT_LIST="xpath://lib.ui.android.widget.TextView[@resource-id='org.wikipedia:id/page_list_item_title']";
